7eb53f6efb4fcf7fc61bfec29e400f4acb5b4b60
xumingqiang <2231371788@qq.com>
2020-12-04 03:57:09 +0800

    [ADD]补充二维数组最大子数组的和

---
+++ lib/src/main/java/com/example/子数组最大和/Solution.java
@@ -0,0 +1,36 @@
+package com.example.子数组最大和;
+
+/**
+ * @author xumingqiang@keepreal.group
+ * @description TODO
+ * @date 2020/12/4 3:56 AM
+ */
+//输入: [-2,1,-3,4,-1,2,1,-5,4],
+//输出: 6
+//解释: 连续子数组 [4,-1,2,1] 的和最大，为 6。
+
+public class Solution {
+    public static void main(String []args) {
+        int[] test = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+        int a = maxSubArray(test);
+        System.out.print(a);
+    }
+
+    public static int maxSubArray(int[] nums){
+        int a = Integer.MIN_VALUE;
+        if(nums == null || nums.length==0) return a;
+
+        int sum = 0;
+
+        for(int i=0;i<nums.length;i++){
+            if(sum>0){
+                sum+=nums[i];
+            } else {
+                sum = nums[i];
+            }
+            a = Math.max(a,sum);
+        }
+
+        return a;
+    }
+}
